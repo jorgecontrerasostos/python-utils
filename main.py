20cf14975d09aaea5b95a56d1a6ee19685da4cf4
Jorge Contreras <jorgecontrerasostos@gmail.com>
2026-02-25 19:02:06 -0600

    refactor: update project structure

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from strings.strings import capitalize_words
+from utils.string_utils import capitalize_words
+from utils.math_utils import is_odd
 
 def main():
     print(capitalize_words("hello world"))
+    print(is_odd(4))
 
 
 if __name__ == "__main__":
